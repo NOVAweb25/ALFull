7a3dc9f40270cf27cb536395b9760a394de5dc52
NOVAweb25 <novaweb@outlook.sa>
2025-11-07 17:27:55 +0300

    W

--- app/home/about.tsx
+++ app/home/about.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { motion } from "framer-motion";
 import Image from "next/image";
-import logo from "@/./public/logo.svg";
+import logo from "@/./public/logo.png";
 import React from "react";
 
 export default function AboutSection() {
